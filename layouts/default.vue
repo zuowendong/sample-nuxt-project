0b801834f599da2b3f1f2d888a7deed8bffca3c8
zwd <17326195691@163.com>
2024-03-11 16:27:37 +0800

    chore: init project

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="">
+    <slot></slot>
+  </div>
+</template>
+
+<script></script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>{{ homeMsg }}</div>
+</template>
+
+<script setup lang="ts">
+import { useHomeStore } from '~/store/home';
+const { homeMsg } = useHomeStore()
+</script>
