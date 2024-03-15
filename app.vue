8e06775b56993a108d622efb8361fa8f3c9d0427
zwd <17326195691@163.com>
2024-03-15 13:50:09 +0800

    feat: seo config

--- app.vue
+++ app.vue
@@ -4,4 +4,13 @@
   </NuxtLayout>
 </template>
 
-<script></script>
+<script setup lang="ts">
+
+
+useSeoMeta({
+  title: 'my nuxt demo',
+  description: '这是一个Nuxt3示例项目, 从0-1搭建',
+  keywords: 'Nuxt3,项目'
+})
+
+</script>
